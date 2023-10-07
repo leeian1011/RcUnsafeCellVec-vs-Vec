5d096f22759823784b84fb6f70b608dc225fc7cf
lee <leeian1011@gmail.com>
2023-10-07 17:31:35 +0800

    added cmdline argument to run

--- src/bin/rc_uc_hashmap.rs
+++ src/bin/rc_uc_hashmap.rs
@@ -2,16 +2,33 @@ use timer::register_logging;
 use timer::ExampleCell;
 use timer::ExampleObject;
 
-use std::{collections::HashMap, rc::Rc, time::UNIX_EPOCH};
+use std::{collections::HashMap, env, rc::Rc, time::UNIX_EPOCH};
+const ARGUMENT_COUNT: usize = 3;
+
 fn main() {
+    let arguments: Vec<String> = env::args().collect();
+    if arguments.len() != ARGUMENT_COUNT {
+        println!("Usage: cargo r --release --bin vec [vector-size] [hashmap-entry-size]");
+        panic!();
+    }
+
+    let vector_size: usize = arguments[1].clone().parse().expect("no vector size");
+    if vector_size < 1 {
+        panic!("minimum vec size of 1");
+    }
+    let hashmap_entry_count: usize = arguments[2].clone().parse().expect("no hashmap entry");
+    if hashmap_entry_count < 1 {
+        panic!("minimum entry size of 1");
+    }
+
     let file_appender = tracing_appender::rolling::minutely("./logs/", "rc-uc-hashmap.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     let _ = register_logging(non_blocking);
 
     let mut arc_uc_hashmap: HashMap<usize, Rc<ExampleCell<Vec<ExampleObject>>>> = HashMap::new();
 
-    for i in 0..100 {
-        arc_uc_hashmap.insert(i, ExampleObject::generate_rr_vec(100));
+    for i in 0..hashmap_entry_count {
+        arc_uc_hashmap.insert(i, ExampleObject::generate_rr_vec(vector_size));
     }
 
     loop {
--- src/bin/vec_hashmap.rs
+++ src/bin/vec_hashmap.rs
@@ -1,17 +1,34 @@
+use std::env;
 use std::{collections::HashMap, time::UNIX_EPOCH};
 
 use timer::register_logging;
 use timer::ExampleObject;
+const ARGUMENT_COUNT: usize = 3;
 
 fn main() {
+    let arguments: Vec<String> = env::args().collect();
+    if arguments.len() != ARGUMENT_COUNT {
+        println!("Usage: cargo r --release --bin vec [vector-size] [hashmap-entry-size]");
+        panic!();
+    }
+
+    let vector_size: usize = arguments[1].clone().parse().expect("no vector size");
+    if vector_size < 1 {
+        panic!("minimum vec size of 1");
+    }
+    let hashmap_entry_count: usize = arguments[2].clone().parse().expect("no hashmap size");
+    if hashmap_entry_count < 1 {
+        panic!("minimum entry size of 1");
+    }
+
     let file_appender = tracing_appender::rolling::minutely("./logs/", "normal_hashmap.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     let _ = register_logging(non_blocking);
 
     let mut normal_hashmap: HashMap<usize, Vec<ExampleObject>> = HashMap::new();
 
-    for i in 0..100 {
-        normal_hashmap.insert(i, ExampleObject::generate_vec(100));
+    for i in 0..hashmap_entry_count {
+        normal_hashmap.insert(i, ExampleObject::generate_vec(vector_size));
     }
 
     loop {
